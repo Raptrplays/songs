2c5a07496f1390232e9dcd71d7bf85c3b65845c4
giulia <murariu.giulia@gmail.com>
2023-10-11 12:18:06 +0200

    responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,21 +8,21 @@
     <title>Index</title>
 </head>
 <body class="bg-neutral-950">
-    <div class="grid grid-cols-2 gap-4">
+    <div class="md:flex md:flex-col sm:flex sm:flex-col">
         <div>
             <table class="w-full border-collapse">
                 <thead>
                     <tr>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             NAME
                         </th>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             YEAR
                         </th>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             TIMES SOLD
                         </th>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             ACTIONS
                         </th>
                     </tr>
@@ -30,16 +30,16 @@
                 <tbody class="bg-neutral-900 border-y-2 text-white">
                     @foreach ($albums as $album)
                         <tr>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 {{ $album->name }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 {{ $album->year }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 {{ $album->times_sold }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 <a href="{{ url('album/' . $album->id . '/edit' )}}" class="text-blue-600">Edit</a>
                                 <form action="{{ url('album/' . $album->id) }}" method="post">
                                     @csrf
@@ -49,27 +49,27 @@
                             </td>
                         </tr>
                     @endforeach
-                </tbody>        
+                </tbody>
             </table>
             <button class="bg-green-500 text-white py-2 px-4 m-3 rounded">
                 <a href="album/create">Create</a>
-              </button>
+            </button>
         </div>
 
         <div>
             <table class="w-full border-collapse">
                 <thead>
                     <tr>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             NAME
                         </th>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             GENRE
                         </th>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             FOUNDED
                         </th>
-                        <th class="px-6 py-3 text-left text-gray-500">
+                        <th class="px-4 py-3 text-left text-gray-500">
                             ACTIONS
                         </th>
                     </tr>
@@ -77,16 +77,16 @@
                 <tbody class="bg-neutral-900 border-y-2 text-white">
                     @foreach ($bands as $band)
                         <tr>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 {{ $band->name }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 {{ $band->genre }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 {{ $band->founded }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-4 py-3">
                                 <a href="{{ url('band/' . $band->id . '/edit') }}" class="text-blue-600">Edit</a>
                                 <form action="{{ url('band/' . $band->id) }}" method="post">
                                     @csrf
@@ -96,11 +96,11 @@
                             </td>
                         </tr>
                     @endforeach
-                </tbody>        
+                </tbody>
             </table>
             <button class="bg-green-500 text-white py-2 px-4 m-3 rounded">
                 <a href="band/create">Create</a>
-              </button>
+            </button>
         </div>
     </div>
 </body>
